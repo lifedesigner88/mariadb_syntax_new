edf0d6cf201502eb6c17b3b415bfbe1f717c8130
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-11-17 10:52:24 +0900

    2023.11.17 2교시

--- base/2023.11.16/3.DML.sql
+++ base/2023.11.16/3.DML.sql
@@ -19,3 +19,25 @@
 -- DELETE FROM
     DELETE FROM [테이블 이름] WHERE [필드이름=데이터값]
     WHERE 절을 생략하면, 해당 테이블에 저장된 
+
+
+-- 이름 중복제거하고 조회(그냥 조회)
+
+    SELECT DISTINCT NAME FROM author;
+
+-- 정렬
+
+    SELECT name FROM author ORDER BY name;
+    SELECT * FROM author ORDER BY name, email desc;    
+
+    SELECT [COLUMS] FROM [TABLE] ORDER BY [COLUMNS (ASC,DESC)] LIMIT;
+    기본 설정 오름차순(ASC)이며, 내림차순시 (DESC)
+    LIMIT(제한)
+
+
+-- 별칭( alias ) 이용한 처리
+
+    SELECT name as AS 이름 FROM author
+    SELECT name as
+
+

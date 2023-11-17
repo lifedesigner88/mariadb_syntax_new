df052241a8ce549314a60bcc050de80eb086cac3
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-11-17 11:50:28 +0900

    2023.11.17 3교시

--- base/2023.11.16/1.DDL.sql
+++ base/2023.11.16/1.DDL.sql
@@ -51,20 +51,6 @@ SHOW FULL COLUMNS FROM author;
 -- 테이블 생성문 조회
 SHOW CREATE TABLE posts;
 
--- CREATE TABLE `posts` (
-   
---     `id`         int(11)         NOT NULL,
---     `title`      varchar(255)    DEFAULT NULL,
---     `content`    varchar(255)    DEFAULT NULL,
---     `author_id`  int(11)         DEFAULT NULL,
-   
---     PRIMARY      KEY (`id`),
---     KEY          `author_id` (`author_id`),
-
---     CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
---     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
-
-
 
 
 -- 테이블 제약조건 조회 
@@ -141,3 +127,20 @@ DROP TABLE POST;
 
 INSERT INTO author(id, name, email) values(1, 'kim', 'abc@naver.com')
 select * from author;       // 보기.
+
+
+
+SELECT * FROM AUTHOR;
+DESCRIBE author;
+CREATE TABLE post(
+	id			int		primary KEY,
+    title		varchar(255),
+    contents	varchar(3000),
+    author_id	int,
+    
+    foreign key(author_id)
+    REFERENCES AUTHOR(id)
+    );
+DESCRIBE post;
+
+ALTER TABLE author MODIFY COLUMN name varchar(100) not null;

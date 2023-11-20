da9dc5b8c1c5547d7290057eb61ea04abe592c49
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-11-20 16:55:45 +0900

    2023.11.20 7교시

--- base/2023.11.20/6.DML_priactice.sql
+++ base/2023.11.20/6.DML_priactice.sql
@@ -25,6 +25,8 @@ SELECT id, title, contents , if (author_id=1, 'first author', 'orther') as autho
 
 
 
+-- 코드를 입력하세요
+
 SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
 
 CASE STATUS
@@ -36,3 +38,12 @@ END
 as STATUS FROM USED_GOODS_BOARD 
 WHERE CREATED_DATE='2022-10-05' 
 ORDER BY BOARD_ID DESC;
+
+
+
+-- 12세 이하인 여자 환자 목록 출력하기. 
+SELECT PT_NAME, PT_NO, GEND_CD, AGE, 
+IF(TLNO IS NULL, 'NONE', TLNO) AS TLNO 
+FROM PATIENT
+WHERE AGE <= 12 AND GEND_CD='W'
+ORDER BY AGE DESC, PT_NAME ASC

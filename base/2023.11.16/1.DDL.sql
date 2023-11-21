b7f7c7a9ce7c140866ff1b930606ffb4923825fd
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-11-21 09:01:03 +0900

    2023.11.20 0st

--- base/2023.11.16/1.DDL.sql
+++ base/2023.11.16/1.DDL.sql
@@ -1,6 +1,6 @@
+-- 데이터 베이스 시작.
 mariadb -u root -p      //p는 패스워드 생략  
 show databases;
-
 
 
 -- 데이터 베이스 생성
@@ -12,7 +12,7 @@ USE board;
 
 
 -- author 테이블 신규 생성
-CREATE TABLE authors(
+CREATE TABLE author(
     id          INT,
     name        VARCHAR(255),
     email       VARCHAR(255), 
@@ -22,9 +22,24 @@ CREATE TABLE authors(
     PRIMARY     KEY(id)
     );
 
-
 -- 테이블 목록 조회
 SHOW TABLES;
+
+-- 테이블 생성문 조회
+SHOW CREATE TABLE author;
+
+CREATE TABLE `author` (
+   `id`             int(11)                 NOT NULL,
+   `name`           varchar(100)            NOT NULL,
+   `email`          varchar(255)                        DEFAULT NULL,
+   `password`       varchar(255)                        DEFAULT NULL,
+   `role`           enum('user','admin')    NOT NULL    DEFAULT 'user',
+   `address`        varchar(255)                        DEFAULT NULL,
+   `age`            tinyint(3) unsigned                 DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `email_unique` (`email`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
 
 
 -- 테이블 조회
@@ -35,7 +50,7 @@ DROP TABLE posts;
 
 
 -- post 테이블 신규생성
-CREATE TABLE posts(
+CREATE TABLE post(
     id          INT PRIMARY KEY,
     title       VARCHAR(255),
     content     VARCHAR(255),
@@ -44,33 +59,35 @@ CREATE TABLE posts(
     FOREIGN     KEY(author_id)  REFERENCES author(id)
     );
 
+-- 테이블 생성문 조회
+SHOW CREATE TABLE author;
+
+CREATE TABLE `post` (
+   `id`             int(11)         NOT NULL    AUTO_INCREMENT,
+   `title`          varchar(255)    NOT NULL,
+   `contents`       varchar(3000)               DEFAULT NULL,
+   `author_id`      int(11)                     DEFAULT NULL,
+   `price`          decimal(10,3)               DEFAULT NULL,
+   `created_time`   datetime(6)                 DEFAULT current_timestamp(6),
+   PRIMARY KEY (`id`),
+   KEY `post_update` (`author_id`),
+   CONSTRAINT `post_update` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON UPDATE SET NULL
+ ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+
 -- 테이블 컬럼 상세조회
 SHOW FULL COLUMNS FROM author;
 
 
--- 테이블 생성문 조회
-SHOW CREATE TABLE posts;
-
-
-
 -- 테이블 제약조건 조회 
-SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = 'posts';
+SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = 'post';
 
 
 -- 테이블 index 조회
-SHOW INDEX FROM posts;
+SHOW INDEX FROM post;
 
 
--- DDL - CREATE 
-
--- 테이블 이름 변경
- ALTER TABLE 테이블명 RENAME 새로운 테이블명
-
-
-
---  컬럼 추가 (ADD)
---     ALTER TABLE 테이블명 ADD COLUMN 컬럼명 자료형 [NULL 또는 NOTNULL]
---     ALTER TABLE author  ADD COLUMN c
+-- ALTER(테이블열 수정)
 
 
 

dfd540e2cb192c6140f19d467fb7b73ef87ecc8b
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-11-22 10:52:13 +0900

    2023.11.22 2ST

--- base/2023.11.22/8.DML_prectice2.sql
+++ base/2023.11.22/8.DML_prectice2.sql
@@ -5,3 +5,22 @@
 
 VARCHAR     ->  B-TREE INDEX 
 TEXT        ->  FULL TEXT INDEX
+
+index 걸면 걸지 않은 것에 비해, 조회속도는 올라가지만 삽입 속도는 느려진다. 
+카디널리티,
+
+=> 내가 직접 INEDX를 만들 때, 
+
+
+
+SHOW INDEX FROM author;
+SHOW INDEX FROM post;
+
+CREATE INDEX index_name ON author(name);
+CREATE INDEX index_name ON author(name, email);
+
+
+SELECT * FROM author WHERE name ='sejng' OR email = '@nl';
+SELECT * FROM author WHERE name ='sejng' AND email = '@nl';
+
+

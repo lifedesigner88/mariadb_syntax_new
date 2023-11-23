16cfb80158a7501adbbf10ef302eddf1eaa3f309
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-11-23 10:00:01 +0900

    2023.11.23 1st

--- base/2023.11.22/dump_practice.sh
+++ base/2023.11.22/dump_practice.sh
@@ -2,3 +2,21 @@ cd
 cd mariadb_syntax_new/project
 sudo mariadb -u root -p
 sudo mysql -u root -p board < dumpfile.sql
+
+sudo apt-get -u update
+sudo apt-get -y upgrade
+sodu apt-get install -y mariadb-server
+
+sudo systemctl status mariadb
+sudo systemctl start mariadb
+
+sudo mariadb -u root -p
+exit
+
+
+git clone https://github.com/lifedesigner88/mariadb_syntax_new.git
+
+
+mysqldump -u root -p --default-character-set=utf8mb4 board > dumpfile.sql
+mysql -u root -p board < dumpfile.sql
+
--- base/2023.11.23/9.DUMP.sql
+++ base/2023.11.23/9.DUMP.sql
@@ -1,2 +1,47 @@
 sodo mariadb -u root -p
 CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'testpw';
+
+
+
+sudo apt-get -u update
+sudo apt-get -y upgrade
+sodu apt-get install -y mariadb-server
+
+sudo systemctl status mariadb
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+
+
+
+cd
+cd mariadb_syntax_new/project
+sudo mariadb -u root -p
+sudo mysql -u root -p board < dumpfile.sql
+
+sudo apt-get -u update
+sudo apt-get -y upgrade
+sodu apt-get install -y mariadb-server
+
+sudo systemctl status mariadb
+sudo systemctl start mariadb
+
+sudo mariadb -u root -p
+exit
+
+
+git clone https://github.com/lifedesigner88/mariadb_syntax_new.git
+
+
+mysqldump -u root -p --default-character-set=utf8mb4 board > dumpfile.sql
+mysql -u root -p board < dumpfile.sql
+
+
+
+-- 폰트 변경
+sudo apt install fontconfig
+sudo fc-cache -f -v
+fc-list | grep -i nanum
+fc-list
+
+scp : 파일 전송 관련 명령어. 

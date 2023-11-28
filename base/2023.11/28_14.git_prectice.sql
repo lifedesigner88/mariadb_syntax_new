7cc15444a2c7c0b608c9842d85919b66aa654050
sejong <lifedesigner88@gmail.com>
2023-11-28 17:51:17 +0900

    2023.11.28 8st

---
+++ base/2023.11/28_14.git_prectice.sql
@@ -0,0 +1,28 @@
+
+add
+commit
+push
+
+pull
+merge
+branch
+checkout       =>        새로운 브랜치로 가겠다. 
+
+git checkout -b branch1  =>    리눅스에서 명령을 해야 하는 상황이 있다. 
+pull request    =>    다른 브렌치의 개발을 하라고 시킴;
+
+
+
+
+소스트리. 
+https://www.sourcetreeapp.com/
+
+
+
+git:https://github.com
+
+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
+
+만료기간 없는 토큰.
+
+

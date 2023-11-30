5ffef818775bf7bc004b4fed9d85994a5c0257e0
sejong <lifedesigner88@gmail.com>
2023-11-30 11:53:43 +0900

    2023.11.33 4st

---
+++ base/2023.11/30_16.git.sql
@@ -0,0 +1,66 @@
+git clone repository
+
+
+git init
+
+git remove add origin [origin]
+
+
+
+
+git push --all 
+- 모든 브랜치의 커밋 이력 push
+
+main <- dev 합친다. 
+
+
+
+
+
+git stash list
+    - 작업저장목록
+
+git stash show 인덱스
+    - 복사본 내용 조회
+    - 상세본 git stash show 0 -p
+
+git stash pop
+    - 작업목록에서 제거하면서 저장사항 적용
+
+git stash apply
+    - 작업목록에서 놔둔채 저장사항 적용
+    - git stash apply 0
+        * 가장 최근에 저장한 stash의 적용
+
+
+git stash clear
+
+
+
+
+
+1.03
+
+major . minor  fetch
+
+git tag v1.01
+git push origin v1.01   
+
+
+
+실제 현업에선 branch1, branch2, main 등 여러 브랜치로 관리
+
+produticon main / dev 개발용. 
+
+
+Pull Request 를 통해 dev merge
+dev에서 main 으로 최종 merge
+
+
+
+
+
+
+
+
+
